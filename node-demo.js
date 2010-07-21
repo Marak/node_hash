491ccfd2bb54abf08f9041ce2bdd46839c119e45
Marak Squires <marak.squires@gmail.com>
2010-07-20 22:25:07 -0400

    got sha1 working, no base64

--- node-demo.js
+++ node-demo.js
@@ -8,10 +8,19 @@ var user_password = "password";
 // don't expose your salt
 var salt = "sUp3rS3CRiT$@lt";
 
-var plainMD5 = hash.md5( user_password );
-sys.puts(plainMD5);
 
-var saltedMD5 = hash.md5( user_password, salt);
-sys.puts(saltedMD5);
+/****** md5 ******/
+var md5 = hash.md5( user_password );
+sys.puts(md5);
+
+var salted_md5 = hash.md5( user_password, salt);
+sys.puts(salted_md5);
+
+/****** sha1 ******/
+var sha1 = hash.sha1( user_password );
+sys.puts(sha1);
+
+var salted_sha1 = hash.sha1( user_password, salt);
+sys.puts(salted_sha1);
 
 
